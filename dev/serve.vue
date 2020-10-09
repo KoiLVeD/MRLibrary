f71112fe946ca55cc01cd26dabfedeec863c088e
Nikolay Dedov <n.n.dedov@yandex.ru>
2020-10-09 17:31:39 +0700

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { MrlibrarySample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    MrlibrarySample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <mrlibrary-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as MrlibrarySample } from './mrlibrary-sample.vue';
